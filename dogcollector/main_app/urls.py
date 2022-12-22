1b8ace67eb5f702b38838b89fc997d1332b631be
00buku <clydescottwilliamsjr@gmail.com>
2022-12-22 15:52:53 -0600

    created model, superuser/admin, implemented details page

--- dogcollector/main_app/urls.py
+++ dogcollector/main_app/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('dogs/', views.dogs_index, name='index'),
+    path('dogs/<int:dog_id>/', views.dogs_detail, name='detail'),
 ]
 
 
--- dogcollector/main_app/views.py
+++ dogcollector/main_app/views.py
@@ -1,26 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Dog
 
-
-class Dog:
-    def __init__(self, name, breed, description, age):
-        self.name=name
-        self.breed=breed
-        self.description=description
-        self.age=age
-
-dogs = [
-    Dog('Cyrus', 'German Shepard', 'Quiet and well mannered', 5),
-    Dog('Pistol', 'Black Lab', 'Playful and fun', 3),
-    Dog('Whiskey', 'Great Pyranese', 'Lazy and sleepy', 6)
-]
 
 # Create your views here.
 def home(request):
-  return HttpResponse('<h1>Dog Collector</h1>')
+    return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def dogs_index(request):
+    dogs = Dog.objects.all()
     return render(request, 'dogs/index.html', {'dogs': dogs})
+
+
+def dogs_detail(request, dog_id):
+  dog = Dog.objects.get(id=dog_id)
+  return render(request, 'dogs/detail.html', {'dog': dog})
